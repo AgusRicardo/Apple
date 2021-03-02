7e791a3aa34859fefc3165e807bb6adb4e6fb713
Agustin Ricardo <agustinricardo14@gmail.com>
2021-03-02 16:42:12 -0300

    se agregaron cosas al JSON y img al index

--- js/productos.js
+++ js/productos.js
@@ -7,6 +7,7 @@ let pageMusic = document.getElementById('pageMusic');
 let loading = document.getElementById('loading');
 let footer = document.querySelector('footer');
 let pageIndex = document.getElementById('pageIndex')
+let iconosGrandes = document.getElementById('iconosGrandes')
 
 // Mostrar Macbooks
 function mostrarMac() {
@@ -39,9 +40,28 @@ fetch('baseDeDatos.json')
             <img src="${(res.macbooks[i]).imagen}" class="card-img-top card-img-top-products" alt="...">
             <div class="card-body">
             <h4 class="card-title">${(res.macbooks[i]).nombre}</h4>
+            </br>
             <h5 class="card-text">$${(res.macbooks[i]).precio}</h5>
             <p>Cantidad en stock: ${(res.macbooks[i]).stock}</p>
             <a  class="btn btn-primary" onclick='agregarAlCarrito(${JSON.stringify(res.macbooks[i])})'>Agregar al carrito</a>
+            <fieldset class="fieldsetWatch">
+            <h1>${(res.macbooks[i]).pulgadas}”</h1>
+            <h3>${(res.macbooks[i]).procesador}
+            <p>Procesador</p></h3>
+            <h3>${(res.macbooks[i]).ram}
+            <p>RAM</p>
+            </h3>
+            <h3>${(res.macbooks[i]).memory}
+            <p>Almacenamiento</p>
+            </h3>
+            <h3>${(res.macbooks[i]).batteryLife}
+            <p>Duración de la batería</p>
+            </h3>
+            <div class="infoTouchId"></div>
+            <p>Touch ID</p>
+            <div class="infoTouchBar"></div>
+            <p>Touch Bar</p>
+            </fieldset>
             </div>
             </div>
             `;
@@ -82,9 +102,19 @@ fetch('baseDeDatos.json')
             <img src="${(res.ipad[i]).imagen}" class="card-img-top card-img-top-products" alt="...">
             <div class="card-body">
             <h4 class="card-title">${(res.ipad[i]).nombre}</h4>
+            </br>
             <h5 class="card-text">$${(res.ipad[i]).precio}</h5>
             <p>Cantidad en stock: ${(res.ipad[i]).stock}</p>
             <a  class="btn btn-primary" onclick='agregarAlCarrito(${JSON.stringify((res.ipad[i]))})'>Agregar al carrito</a>
+            <fieldset class="fieldsetWatch">
+            <h1>${(res.ipad[i]).pulgadas}”</h1>
+            <h3>${(res.ipad[i]).memory}<p>Memoria</p></h3>
+            <h3><img src="${(res.ipad[i]).camara}"><p>Camara</p></h3>
+            <div class="infoKey"></div>
+            <p>Compatible con Magic Keyboard, Smart Keyboard Folio</p>
+            <div class="infoPencil"></div>
+            <p>Compatible con Apple Pencil (segunda generación)</p>
+            </fieldset>
             </div>
             </div>
             `;
@@ -127,9 +157,27 @@ fetch('baseDeDatos.json')
             <img src="${(res.iphone[i]).imagen}" class="card-img-top card-img-top-products" alt="...">
             <div class="card-body">
             <h4 class="card-title">${(res.iphone[i]).nombre}</h4>
+            </br>
             <h5 class="card-text">$${(res.iphone[i]).precio}</h5>
             <p>Cantidad en stock: ${(res.iphone[i]).stock}</p>
             <a  class="btn btn-primary" onclick='agregarAlCarrito(${JSON.stringify((res.iphone[i]))})'>Agregar al carrito</a>
+            <fieldset class="fieldsetWatch">
+            <h1>${(res.iphone[i]).pulgadas}”</h1>
+            <h4>${(res.iphone[i]).memory}<p>Memoria</p></h4>
+            <h3><img src="${(res.iphone[i]).camara}"><p>Camara</p></h3>
+            <div class="info5G"></div>
+            <p>Descargas ultrarrápidas, transmisión de alta calidad</p>
+            <div class="infoA14"></div>
+            <p>Chip biónico A14</p>
+            <div class="infoBateria"></div>
+            <p>Hasta 17 horas de reproducción de video</p>
+            <div class="infoID"></div>
+            <p>ID de rostro</p>
+            <div class="infoWater"></div>
+            <p>Resistente al agua hasta una profundidad de 6 metros por hasta 30 minutos</p>
+            <div class="infoAcc"></div>
+            <p>Compatible con accesorios MagSafe y cargadores inalámbricos</p>
+            </fieldset>
             </div>
             </div>
             `;
@@ -171,10 +219,13 @@ fetch('baseDeDatos.json')
             <img src="${(res.watch[i]).imagen}" class="card-img-top card-img-top-products" alt="...">
             <div class="card-body">
             <h4 class="card-title">${(res.watch[i]).nombre}</h4>
+            </br>
             <h5 class="card-text">$${(res.watch[i]).precio}</h5>
             <p>Cantidad en stock: ${(res.watch[i]).stock}</p>
             <a  class="btn btn-primary" onclick='agregarAlCarrito(${JSON.stringify((res.watch[i]))})'>Agregar al carrito</a>
             <fieldset class="fieldsetWatch">
+            <div class="infoCaseSize"></div>
+            <p>${(res.watch[i]).medidas}</p>
             <div class="infoWatchWater"></div>
             <p>Resistencia al agua</p>
             <div class="infoWatchHeart"></div>
@@ -185,6 +236,7 @@ fetch('baseDeDatos.json')
             <p>Emergencia SOS</p>
             <div class="infoWatchAltimetro"></div>
             <p>Altímetro</p>
+            <h4>${(res.watch[i]).materiales}<p>Materiales</p></h4>
             </fieldset>
             <div class="card-text"></div>
             </div>
@@ -228,9 +280,13 @@ fetch('baseDeDatos.json')
             <img src="${(res.music[i]).imagen}" class="card-img-top card-img-top-products" alt="...">
             <div class="card-body">
             <h4 class="card-title">${(res.music[i]).nombre}</h4>
+            </br>
             <h5 class="card-text">$${(res.music[i]).precio}</h5>
             <p>Cantidad en stock: ${(res.music[i]).stock}</p>
-            <a  class="btn btn-primary" onclick='agregarAlCarrito(${JSON.stringify((res.music[i]))})'>Agregar al carrito</a>
+            <a  class="btn btn-primary" onclick='agregarAlCarrito(${JSON.stringify((res.music[i]))})'><i class="fas fa-shopping-bag"></i> Agregar al carrito <i class="fas fa-angle-right"></i></a>
+            <fieldset class="fieldsetWatch">
+            <h3>${(res.music[i]).battery}<p>Tiempo de escucha con una carga</p></h3>
+            </fieldset>
             </div>
             </div>
             `;
